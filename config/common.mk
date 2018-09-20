23e04e4b9f02196a05e95aeea87b58d94db93ef9
sdhz150 <yanghui150@163.com>
2018-09-20 12:19:22 +0800

    Use all private libraries "libsuda-phoneloc-jni.so libsudautils.so"
    Change-Id: I46c922fac334ada38baee1ae2212b17e1c0193b1

--- config/common.mk
+++ config/common.mk
@@ -67,6 +67,16 @@ PRODUCT_COPY_FILES += \
     vendor/havoc/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
 endif
 
+# Use all private libraries
+ifeq ($(SUDA_CPU_ABI),arm64-v8a)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*.so,vendor/havoc/prebuilt/suda/lib/$(SUDA_CPU_ABI),system/lib64) \
+    $(call find-copy-subdir-files,*.so,vendor/havoc/prebuilt/suda/lib/armeabi-v7a/,system/lib)
+else
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*.so,vendor/havoc/prebuilt/suda/lib/$(SUDA_CPU_ABI),system/lib)
+endif
+
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \
     vendor/havoc/config/permissions/backup.xml:system/etc/sysconfig/backup.xml

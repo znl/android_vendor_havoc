sdhz152/android_vendor_lineage

Use all private libraries "libsuda-phoneloc-jni.so libsudautils.so"
Change-Id: I46c922fac334ada38baee1ae2212b17e1c0193b1

config/common.mk
@@ -56,6 +56,16 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
 endif
 
+# Use all private libraries
+ifeq ($(SUDA_CPU_ABI),arm64-v8a)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*.so,vendor/lineage/prebuilt/suda/lib/$(SUDA_CPU_ABI),system/lib64) \
+    $(call find-copy-subdir-files,*.so,vendor/lineage/prebuilt/suda/lib/armeabi-v7a/,system/lib)
+else
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*.so,vendor/lineage/prebuilt/suda/lib/$(SUDA_CPU_ABI),system/lib)
+endif
+
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/backup.xml:system/etc/sysconfig/backup.xml

znl/android_vendor_havoc

config/common.mk
@@ -67,6 +67,16 @@ PRODUCT_COPY_FILES += \
     vendor/havoc/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
 endif
 
+# Use all private libraries
+ifeq ($(SUDA_CPU_ABI),arm64-v8a)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*.so,vendor/havoc/prebuilt/suda/lib/$(SUDA_CPU_ABI),system/lib64) \
+    $(call find-copy-subdir-files,*.so,vendor/havoc/prebuilt/suda/lib/armeabi-v7a/,system/lib)
+else
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*.so,vendor/havoc/prebuilt/suda/lib/$(SUDA_CPU_ABI),system/lib)
+endif
+
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \
     vendor/havoc/config/permissions/backup.xml:system/etc/sysconfig/backup.xml